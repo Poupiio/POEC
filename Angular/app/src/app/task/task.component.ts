8d191a18a9710ccb758c607c21a67b2d36c8095e
Elise <elise.maaiza33@gmail.com>
2024-04-15 14:41:06 +0200

    🦺 Tried ngModel

--- Angular/app/src/app/task/task.component.ts
+++ Angular/app/src/app/task/task.component.ts
@@ -9,26 +9,54 @@ import { FormBuilder } from '@angular/forms';
   ]
 })
 export class TaskComponent implements OnInit {
-
+  title : string = "";
+  description : string ="";
+  status : string = "TODO";
+  estimation : number = 1;
+  projectId : number = 1;
   
 
-  constructor(private formBuilder : FormBuilder) { }
+  // constructor(private formBuilder : FormBuilder) { }
+  constructor() { }
 
-  public taskForm = this.formBuilder.group({
-    title : "",
-    description :"",
-    status : "TODO",
-    estimation : new Date(),
-    project : 0,
-  })
+  // public taskForm = this.formBuilder.group({
+  //   title : "",
+  //   description : "",
+  //   status : "TODO",
+  //   estimation : "1",
+  //   project : 1,
+  // })
+
+  // submit() : void {
+  //   console.log("Submit cliqué ouiiiii");
+  //   const title = this.taskForm.value.title;
+  //   const description = this.taskForm.value.description;
+  //   const status = this.taskForm.value.status;
+  //   const estimation = this.taskForm.value.estimation;
+  //   const project = this.taskForm.value.project;
+
+  //   console.log(title, description, status, estimation, project);
+
+  //   // Réinitialisation des inputs du formulaire après la soumission
+  //   this.taskForm.patchValue({
+  //     title: '',
+  //     description: '',
+  //     status: 'TODO',
+  //     estimation: '1',
+  //     project: 1
+  //   });
+  // }
 
   submit() : void {
-    console.log("Submit cliqué ouiiiii");
-    
+    console.log(this.title, this.description, this.status, this.estimation, this.projectId);
+
+    this.title = "";
+    this.description = "";
+    this.status = "TODO";
+    this.estimation = 1;
+    this.projectId = 1;
   }
 
-  ngOnInit(): void {
-    // console.log(this.taskStatus);
-  }
+  ngOnInit(): void {}
 
 }
